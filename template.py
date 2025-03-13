2ddc03c7871d557a313e6c72eb1ad6926ac45601
Shreyas Chigurupati <84034817+shreyas-chigurupati07@users.noreply.github.com>
2025-03-12 18:33:33 -0700

    Added Project structure

--- template.py
+++ template.py
@@ -5,48 +5,41 @@ import logging
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s: %(levelname)s: %(message)s')
 
-
 project_name = 'rufusAgent'
 
 list_of_files = [
-    '.github/workflows/.gitkeep',
-    f'src/{project_name}/__init__.py',
-    f'src/{project_name}/scraper.py',
-    f'src/{project_name}/api.py',
-    f'data/{project_name}/output.json',
-    f'tests/{project_name}/test_scraper.py',
-
-    # f'src/{project_name}/components/__init__.py',
-    # f'src/{project_name}/components/data_ingestion.py',
-    # f'src/{project_name}/components/prepare_base_model.py',
-    # f'src/{project_name}/components/training.py',
-    # f'src/{project_name}/components/evaluation.py',
-    # f'src/{project_name}/components/prepare_callbacks.py',
-    # f'src/{project_name}/utils/__init__.py',
-    # f'src/{project_name}/utils/common.py',
-    # f'src/{project_name}/config/__init__.py',
-    # f'src/{project_name}/config/configuration.py',
-    # f'src/{project_name}/pipeline/__init__.py',
-    # f'src/{project_name}/entity/__init__.py',
-    # f'src/{project_name}/entity/config_entity.py',
-    # f'src/{project_name}/constants/__init__.py',
-    # f'src/{project_name}/pipeline/stage_01_data_ingestion.py',
-    # f'src/{project_name}/pipeline/stage_02_prepare_base_model.py',
-    # f'src/{project_name}/pipeline/stage_03_training.py',
-    # f'src/{project_name}/pipeline/stage_04_evaluation.py',
-    # f'src/{project_name}/pipeline/predict.py',
-    #'config/config.yaml',
-    #'app.py',
-    #'Dockerfile',
-    #'dvc.yaml',
-    #'params.yaml',
+    'Dockerfile',
+    'LICENSE',
+    'README.md',
     'requirements.txt',
     'setup.py',
+    '.env',
+    '.gitignore',
+    'data/raw/.gitkeep',
+    'data/processed/.gitkeep',
+    'data/output.json',
+    'research/exploration.ipynb',
+    'research/web_scraping_tests.ipynb',
+    'tests/__init__.py',
+    'tests/test_scraper.py',
+    'tests/test_api.py',
+    'tests/test_utils.py',
+    f'src/{project_name}/__init__.py',
+    f'src/{project_name}/scraper.py',
+    f'src/{project_name}/processor.py',
+    f'src/{project_name}/api.py',
+    f'src/{project_name}/utils.py',
+    f'src/{project_name}/config.py',
+    f'src/{project_name}/logger.py',
+    f'src/{project_name}/models/__init__.py',
     'main.py',
-    'research/trails.ipynb',
-    #'templates/index.html'
+    'template.py',
+    'deployment/docker-compose.yml',
+    'deployment/k8s/.gitkeep',
+    'deployment/cloud_run.yaml',
+    'deployment/aws_lambda.py',
+    'deployment/heroku.yml'
 ]
-
 
 for filepath in list_of_files:
     filepath = Path(filepath)
@@ -56,15 +49,9 @@ for filepath in list_of_files:
         os.makedirs(filedir, exist_ok=True)
         logging.info(f'Created directory: {filedir} for the file: {filename}')
 
-    if (not os.path.exists(filepath)) or  (os.path.getsize(filepath) == 0):
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, 'w') as f:
             pass
-            logging.info(f'Created empty file: {filename} in the directory: {filedir}')
+        logging.info(f'Created empty file: {filename} in the directory: {filedir}')
     else:
         logging.info(f'File: {filename} already exists in the directory: {filedir}')
-
-
-
-
-
-
